6c3b9833774082917d53349c2203fb59c4146e03
Christian Boettger <christian.boettger@ht16.de>
2012-02-16 13:51:37 +0100

    modify for sheet background

--- templates/block.tpl.php
+++ templates/block.tpl.php
@@ -54,13 +54,36 @@
  */
 ?>
 <div id="<?php print $block_html_id; ?>" class="<?php print $classes; ?>">
-  <?php if ($title): ?>
-    <h2 class="title"><?php print $title; ?></h2>
-  <?php endif; ?>
 
-  <div class="content">
-    <?php print $content; ?>
+  <div class="Block <?php print block_class($block); ?>">
+  <div class="Block-tl"></div>
+  <div class="Block-tr"></div>
+  <div class="Block-bl"></div>
+  <div class="Block-br"></div>
+  <div class="Block-tc"></div>
+  <div class="Block-bc"></div>
+  <div class="Block-cl"></div>
+  <div class="Block-cr"></div>
+  <div class="Block-cc"></div>
+  <div class="Block-body">
+    <?php if ($block->subject): ?>
+      <div class="BlockHeader">
+        <div class="l"></div>
+        <div class="r"></div>
+        <div class="header-tag-icon">
+          <div class="t">
+            <?php echo $block->subject; ?>
+          </div>
+        </div>
+      </div>
+    <?php endif; ?>
+    <div class="BlockContent">
+      <div class="BlockContent-body">
+        <?php echo $block->content; ?>
+      </div>
+    </div>
   </div>
+</div>
 
   <?php print $edit_links; ?>
 </div><!-- /.block -->
