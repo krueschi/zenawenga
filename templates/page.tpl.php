8e51c14bc64127b72e18155408406d89c5c3a3c3
Christian Boettger <christian.boettger@ht16.de>
2012-12-23 12:22:05 +0100

    Altered page design
    - removed footer background (comment out) from red grafic
    - moved secondary menu from footer to header (top right) hardcoded
    - changed css for secondary menu to small caps with grey shading

--- templates/page.tpl.php
+++ templates/page.tpl.php
@@ -70,6 +70,22 @@
           <?php endif; ?>
         </div><!-- /#name-and-slogan -->
       <?php endif; ?>
+      
+      <?php if($secondary_links): ?>
+        <div id="header-links">
+          <?php print theme(array('links__system_secondary_menu', 'links'), $secondary_links,
+          array(
+            'id' => 'secondary-menu',
+            'class' => 'links clearfix',
+          ),
+          array(
+            'text' => t('Secondary menu'),
+            'level' => 'h2',
+            'class' => 'element-invisible',
+          ));
+        ?>
+        </div>
+      <?php endif; ?>
 
       <?php if ($search_box): ?>
         <div id="search-box"><?php print $search_box; ?></div>
@@ -154,18 +170,6 @@
 
     <?php if ($footer || $footer_message || $secondary_links): ?>
       <div id="footer"><div class="section">
-
-        <?php print theme(array('links__system_secondary_menu', 'links'), $secondary_links,
-          array(
-            'id' => 'secondary-menu',
-            'class' => 'links clearfix',
-          ),
-          array(
-            'text' => t('Secondary menu'),
-            'level' => 'h2',
-            'class' => 'element-invisible',
-          ));
-        ?>
 
         <?php if ($footer_message): ?>
           <div id="footer-message"><?php print $footer_message; ?></div>
